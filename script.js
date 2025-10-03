16b9aacdc00cc39e8caaa0f3f6500a15baf2496e
bengaudry <bengaudry@outlook.fr>
2025-10-03 14:31:13 +0200

    Add: articles list

---
+++ script.js
@@ -0,0 +1,20 @@
+const ARTICLES = [
+  {
+    id: "article-1",
+    titre: "Article 1",
+    resume: "Resumé de l'article 1",
+    contenu: "Contenu de l'article 1",
+  },
+  {
+    id: "article-2",
+    titre: "Article 2",
+    resume: "Resumé de l'article 2",
+    contenu: "Contenu de l'article 2",
+  },
+  {
+    id: "article-3",
+    titre: "Article 3",
+    resume: "Resumé de l'article 3",
+    contenu: "Contenu de l'article 3",
+  },
+];
